e26b9571eed44e7bc7511f4b42266d347250f73a
Autumn Martin <autumn.the.dev@gmail.com>
2023-03-01 22:23:44 -0700

    Begin adding theme

--- client/src/components/Intro/Welcome.jsx
+++ client/src/components/Intro/Welcome.jsx
@@ -1,12 +1,14 @@
 function Welcome() {
   return (
     <div className="welcome">
-      <h1>👋 Welcome to the Truffle + React Box!</h1>
+      <img className="logo" src="logos/logo-square.png" alt="logo" />
+      <h1>👋 Welcome to the <span className="gradient-text">Truffle</span> + React Box!</h1>
       <p>
         This is everything you need to start using Truffle to write,
         compile, test, and deploy smart contracts, and interact with
         them from a React app.
       </p>
+      <button class="gradient-background">Button</button>
     </div>
   );
 }
