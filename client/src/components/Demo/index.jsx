ba7147a1ac54aac3e3073e27a767ff4917eaf88e
Autumn Martin <autumn.the.dev@gmail.com>
2023-03-04 01:02:25 -0700

    Update views for wrong network & no artifact errors
    Display calendar so that users can preview it despite errors, and update
    toast message design.

--- client/src/components/Demo/index.jsx
+++ client/src/components/Demo/index.jsx
@@ -1,25 +1,16 @@
 import { useState } from "react";
 import useEth from "../../contexts/EthContext/useEth";
 import Calendar from "../Calendar";
-import NoticeNoArtifact from "./NoticeNoArtifact";
-import NoticeWrongNetwork from "./NoticeWrongNetwork";
+import Toast from "./Toast";
 
 function Demo() {
   const { state } = useEth();
   const [value, setValue] = useState("");
 
-  const demo =
-    <>
-      <Calendar setValue={setValue} value={value} />
-    </>;
-
   return (
     <div className="demo">
-      {
-        !state.artifact ? <NoticeNoArtifact /> :
-          !state.contract ? <NoticeWrongNetwork /> :
-            demo
-      }
+      <Toast/>
+      <Calendar setValue={setValue} value={value} />
     </div>
   );
 }
